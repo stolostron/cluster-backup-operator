3192782902b643d8a38a4f54beeb6da4c288a86f
sahare <sahare@redhat.com>
2021-08-31 15:02:56 -0400

    Adding tests
    Signed-off-by: sahare <sahare@redhat.com>

--- controllers/schedule_controller_test.go
+++ controllers/schedule_controller_test.go
@@ -15,23 +15,23 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("Backup controller", func() {
+var _ = Describe("BackupSchedule controller", func() {
 
 	var (
 		ctx                 context.Context
 		managedClusters     []clusterv1.ManagedCluster
 		veleroNamespaceName string
 		veleroNamespace     *corev1.Namespace
-		backupName          string = "the-backup-name"
+		backupScheduleName  string = "the-backup-schedule-name"
+		backupSchedule      string = "0 */6 * * *"
 
-		timeout = time.Second * 70
-		//duration = time.Second * 10
+		timeout  = time.Second * 70
 		interval = time.Millisecond * 250
 	)
 
 	BeforeEach(func() {
 		ctx = context.Background()
-		veleroNamespaceName = "velero-backup-ns"
+		veleroNamespaceName = "velero-ns"
 		managedClusters = []clusterv1.ManagedCluster{
 			{
 				TypeMeta: metav1.TypeMeta{
@@ -83,8 +83,8 @@ var _ = Describe("Backup controller", func() {
 		}
 		Expect(k8sClient.Create(ctx, veleroNamespace)).Should(Succeed())
 	})
-	Context("When creating a Backup", func() {
-		It("Should creating a Velero Backup updating the Status", func() {
+	Context("When creating a BackupSchedule", func() {
+		It("Should be creating a Velero Schedule updating the Status", func() {
 			managedClusterList := clusterv1.ManagedClusterList{}
 			Eventually(func() bool {
 				err := k8sClient.List(ctx, &managedClusterList, &client.ListOptions{})
@@ -103,45 +103,45 @@ var _ = Describe("Backup controller", func() {
 				return false
 			}, timeout, interval).Should(BeTrue())
 
-			rhacmBackup := v1beta1.Backup{
+			rhacmBackupSchedule := v1beta1.BackupSchedule{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "cluster.open-cluster-management.io/v1beta1",
-					Kind:       "Backup",
+					Kind:       "BackupSchedule",
 				},
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      backupName,
+					Name:      backupScheduleName,
 					Namespace: veleroNamespaceName,
 				},
-				Spec: v1beta1.BackupSpec{
-					VeleroConfig: &v1beta1.VeleroConfigBackupProxy{
-						Namespace: veleroNamespaceName,
-					},
-					Interval:   60,
-					MaxBackups: 2,
+				Spec: v1beta1.BackupScheduleSpec{
+					VeleroSchedule: "0 */6 * * *",
+					VeleroTTL:      metav1.Duration{Duration: time.Hour * 72},
 				},
 			}
-			Expect(k8sClient.Create(ctx, &rhacmBackup)).Should(Succeed())
+			Expect(k8sClient.Create(ctx, &rhacmBackupSchedule)).Should(Succeed())
 			backupLookupKey := types.NamespacedName{
-				Name:      backupName,
+				Name:      backupScheduleName,
 				Namespace: veleroNamespaceName,
 			}
-			createdBackup := v1beta1.Backup{}
+			createdBackupSchedule := v1beta1.BackupSchedule{}
 			Eventually(func() bool {
-				err := k8sClient.Get(ctx, backupLookupKey, &createdBackup)
+				err := k8sClient.Get(ctx, backupLookupKey, &createdBackupSchedule)
 				return err == nil
 			}, timeout, interval).Should(BeTrue())
 
-			Expect(createdBackup.CreationTimestamp.Time).NotTo(BeNil())
+			Expect(createdBackupSchedule.CreationTimestamp.Time).NotTo(BeNil())
 
-			//By("created backup should contain velero backup in status")
+			Expect(createdBackupSchedule.Spec.VeleroSchedule).Should(Equal(backupSchedule))
+
+			//By("created backup schedule should contain velero schedules in status")
 			Eventually(func() bool {
-				err := k8sClient.Get(ctx, backupLookupKey, &createdBackup)
+				err := k8sClient.Get(ctx, backupLookupKey, &createdBackupSchedule)
 				if err != nil {
 					return false
 				}
-				return createdBackup.Status.VeleroBackups != nil
+				return createdBackupSchedule.Status.VeleroScheduleCredentials != nil &&
+					createdBackupSchedule.Status.VeleroScheduleManagedClusters != nil &&
+					createdBackupSchedule.Status.VeleroScheduleResources != nil
 			}, timeout, interval).ShouldNot(BeNil())
-
 		})
 
 	})
