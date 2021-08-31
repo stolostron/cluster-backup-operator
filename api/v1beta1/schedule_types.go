7c5a68106cd44a8fb8a87942af47d7203a5104db
sahare <sahare@redhat.com>
2021-08-31 15:05:10 -0400

    Remove scaffolding comment
    Signed-off-by: sahare <sahare@redhat.com>

--- api/v1beta1/schedule_types.go
+++ api/v1beta1/schedule_types.go
@@ -22,7 +22,6 @@ import (
 	veleroapi "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // BackupScheduleSpec defines the desired state of BackupSchedule
