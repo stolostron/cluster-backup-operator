b3fbcc8a2401b87848ce89488f3e1363b634b465
sahare <sahare@redhat.com>
2021-09-20 14:07:13 -0400

    Update spec description
    Signed-off-by: sahare <sahare@redhat.com>

--- api/v1beta1/restore_types.go
+++ api/v1beta1/restore_types.go
@@ -23,18 +23,21 @@ import (
 // RestoreSpec defines the desired state of Restore
 type RestoreSpec struct {
 	// VeleroManagedClustersBackupName is the name of the velero back-up used to restore managed clusters.
-	// Is optional, if not supplied, the backup name will be selected
-	// from the available backups in the current namespace
+	// Is required, valid values are latest, skip or backup_name
+	// If value is set to latest, the latest backup is used, skip will not restore this type of backup
+	// backup_name points to the name of the backup to be restored
 	// +kubebuilder:validation:Required
 	VeleroManagedClustersBackupName *string `json:"veleroManagedClustersBackupName"`
 	// VeleroResourcesBackupName is the name of the velero back-up used to restore resources.
-	// Is optional, if not supplied, the backup name will be selected
-	// from the available backups in the current namespace
+	// Is required, valid values are latest, skip or backup_name
+	// If value is set to latest, the latest backup is used, skip will not restore this type of backup
+	// backup_name points to the name of the backup to be restored
 	// +kubebuilder:validation:Required
 	VeleroResourcesBackupName *string `json:"veleroResourcesBackupName"`
 	// VeleroCredentialsBackupName is the name of the velero back-up used to restore credentials.
-	// Is optional, if not supplied, the backup name will be selected
-	// from the available backups in the current namespace
+	// Is required, valid values are latest, skip or backup_name
+	// If value is set to latest, the latest backup is used, skip will not restore this type of backup
+	// backup_name points to the name of the backup to be restored
 	// +kubebuilder:validation:Required
 	VeleroCredentialsBackupName *string `json:"veleroCredentialsBackupName"`
 }
