31ee07c8f1961f2db464cccbed2b46a1ba678ecf
Valentina Birsan <vbirsan@redhat.com>
2022-01-24 13:38:20 -0500

    more tests
    Signed-off-by: Valentina Birsan <vbirsan@redhat.com>

---
+++ controllers/utils_test.go
@@ -0,0 +1,90 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package controllers
+
+import (
+	"testing"
+
+	veleroapi "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Test_isValidStorageLocationDefined(t *testing.T) {
+	type args struct {
+		veleroStorageLocations *veleroapi.BackupStorageLocationList
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "No storage locations",
+			args: args{
+				veleroStorageLocations: &veleroapi.BackupStorageLocationList{
+					Items: make([]veleroapi.BackupStorageLocation, 0),
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Storage locations but no owner reference",
+			args: args{
+				veleroStorageLocations: &veleroapi.BackupStorageLocationList{
+					Items: []veleroapi.BackupStorageLocation{
+						veleroapi.BackupStorageLocation{
+							TypeMeta: metav1.TypeMeta{
+								APIVersion: "velero/v1",
+								Kind:       "BackupStorageLocation",
+							},
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "invalid-location-no-owner-ref",
+								Namespace: "default",
+							},
+							Status: veleroapi.BackupStorageLocationStatus{
+								Phase: veleroapi.BackupStorageLocationPhaseAvailable,
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Storage location valid",
+			args: args{
+				veleroStorageLocations: &veleroapi.BackupStorageLocationList{
+					Items: []veleroapi.BackupStorageLocation{
+						veleroapi.BackupStorageLocation{
+							TypeMeta: metav1.TypeMeta{
+								APIVersion: "velero/v1",
+								Kind:       "BackupStorageLocation",
+							},
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "valid-storage",
+								Namespace: "default",
+								OwnerReferences: []v1.OwnerReference{
+									v1.OwnerReference{
+										Kind: "DataProtectionApplication",
+									},
+								},
+							},
+							Status: veleroapi.BackupStorageLocationStatus{
+								Phase: veleroapi.BackupStorageLocationPhaseAvailable,
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := isValidStorageLocationDefined(*tt.args.veleroStorageLocations); got != tt.want {
+				t.Errorf("isValidStorageLocationDefined() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
