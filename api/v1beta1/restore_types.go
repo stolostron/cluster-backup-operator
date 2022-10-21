b577592964f1940c5cd1bfda3879f15a9435eadd
Valentina Birsan <vbirsan@redhat.com>
2022-10-20 20:40:00 -0400

    make messages optional
    Signed-off-by: Valentina Birsan <vbirsan@redhat.com>

--- api/v1beta1/restore_types.go
+++ api/v1beta1/restore_types.go
@@ -110,9 +110,10 @@ type RestoreStatus struct {
 	// Message on the last operation
 	// +kubebuilder:validation:Optional
 	LastMessage string `json:"lastMessage"`
-	//Messages contains any messages that were encountered during
-	// the restore process.
-	Messages []string `json:"messages"`
+	// Messages contains any messages that were encountered during the restore process.
+	// +optional
+	// +nullable
+	Messages []string `json:"messages,omitempty"`
 }
 
 // +kubebuilder:object:root=true
