75b499f08bcbfee08134dbcdbbb4195b5cecf38d
Valentina Birsan <vbirsan@redhat.com>
2024-01-17 18:36:20 -0500

    fix sonar
    Signed-off-by: Valentina Birsan <vbirsan@redhat.com>

--- controllers/restore_post.go
+++ controllers/restore_post.go
@@ -93,7 +93,8 @@ func cleanupDeltaResources(
 		backupName, veleroBackup := getBackupInfoFromRestore(ctx, c,
 			acmRestore.Status.VeleroCredentialsRestoreName, acmRestore.Namespace)
 		cleanupDeltaForCredentials(ctx, c,
-			backupName, veleroBackup, acmRestore.Spec.CleanupBeforeRestore, *acmRestore.Spec.VeleroManagedClustersBackupName != skipRestoreStr)
+			backupName, veleroBackup, acmRestore.Spec.CleanupBeforeRestore,
+			*acmRestore.Spec.VeleroManagedClustersBackupName != skipRestoreStr)
 
 		// clean up resources and generic resources
 		backupName, veleroBackup = getBackupInfoFromRestore(ctx, c,
