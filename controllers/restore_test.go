a788a0c7f3cf5a60be2c6609c891827ecc9cc3d4
sahare <sahare@redhat.com>
2025-11-05 12:18:56 -0500

    Revert "[release-2.14] Fixes credentials and generic resources restore when using non-sync mode with all backup names set"
    This reverts commit 65c974012b0d4ddaa0e8351371d4db2b52218646.

--- controllers/restore_test.go
+++ controllers/restore_test.go
@@ -38,7 +38,6 @@ import (
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -1432,9 +1431,7 @@ func Test_updateLabelsForActiveResources(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fakeClient := fake.NewClientBuilder().Build()
-			got := updateLabelsForActiveResources(context.Background(), fakeClient,
-				tt.args.acmRestore, tt.args.restype, tt.args.veleroRestoresToCreate)
+			got := updateLabelsForActiveResources(tt.args.acmRestore, tt.args.restype, tt.args.veleroRestoresToCreate)
 			if got != tt.want {
 				t.Errorf("error updating labels for: %s", tt.name)
 			}
